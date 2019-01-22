78cfec5da12d0955bea5b42af6cac6bba7fb3161
Tung Tran <tungtx90@gmail.com>
2019-01-22 21:58:28 +0700

    Add test case for HeadlinesManager

---
+++ SurveyFormTests/APIs/APIHeadlinesTests.swift
@@ -0,0 +1,43 @@
+//
+//  APIHeadlinesTests.swift
+//  SurveyFormTests
+//
+//  Created by Tung Tran on 1/22/19.
+//  Copyright © 2019 tungtran. All rights reserved.
+//
+
+import XCTest
+
+@testable import SurveyForm
+
+class APIHeadlinesTests: XCTestCase {
+    func testMissingCountryParamResponse() {
+        let expectation = XCTestExpectation(description: "testMissingCountryParamResponse")
+        
+        APIManager.Headlines.getTop(country: "") { (response) in
+            switch response.result {
+            case .failure(_):
+                break
+            case .success(_):
+                XCTFail("Request should be failed")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Constant.StringURL.timeoutInterval)
+    }
+    
+    func testValidResponse() {
+        let expectation = XCTestExpectation(description: "testValidResponse")
+        
+        APIManager.Headlines.getTop(country: "us") { (response) in
+            switch response.result {
+            case .failure(_):
+                XCTFail("Request should be successful")
+            case .success(_):
+                break
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Constant.StringURL.timeoutInterval)
+    }
+}
---
+++ SurveyFormTests/Managers/HeadlinesManagerTests.swift
@@ -0,0 +1,43 @@
+//
+//  HeadlinesManagerTests.swift
+//  SurveyFormTests
+//
+//  Created by Tung Tran on 1/22/19.
+//  Copyright © 2019 tungtran. All rights reserved.
+//
+
+import XCTest
+
+@testable import SurveyForm
+
+class HeadlinesManagerTests: XCTestCase {
+    func testEmptyResponse() {
+        let expectation = XCTestExpectation(description: "testEmptyResponse")
+        
+        HeadlinesManager.getTop(country: "abc") { (response) in
+            switch response {
+            case .failure(_):
+                XCTFail("Request should be successful")
+            case .success(let news):
+                XCTAssert(news.isEmpty, "Result should be empty")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Constant.StringURL.timeoutInterval)
+    }
+    
+    func testValidResponse() {
+        let expectation = XCTestExpectation(description: "testValidResponse")
+        
+        HeadlinesManager.getTop(country: "us") { (response) in
+            switch response {
+            case .failure(_):
+                XCTFail("Request should be successful")
+            case .success(let news):
+                XCTAssert(!news.isEmpty, "Result should not be empty")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Constant.StringURL.timeoutInterval)
+    }
+}
